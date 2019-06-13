955da56a941f7cf69f5e744716c8f166b8060605
north <ken.washikita@gmail.com>
2019-06-13 10:51:49 +0900

    update

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ sed -i 's/ja_JP/en_US/' /etc/locale.conf
 sed -i 's/^UsePAM no/UsePAM yes/' /etc/ssh/sshd_config
 
 # logrotate
-sed -i 's/^rotate 4$/rotate 10/' /etc/logrotate.conf
+sed -i '/^rotate 4$/s/4/10/' /etc/logrotate.conf
 
 # vi
 cat <<EOL >/root/.vimrc
