659464de31a62737a65672a22e6e51030b494a4c
north <ken.washikita@gmail.com>
2019-11-15 16:47:29 +0900

    centos8

--- centos8/sacloud-startup.sh
+++ centos8/sacloud-startup.sh
@@ -3,15 +3,15 @@
 # @sacloud-name "Northcave Setup"
 # @sacloud-once
 #
-# @sacloud-require-archive distro-centos distro-ver-7.*
+# @sacloud-require-archive distro-centos distro-ver-8.*
 #
 # @sacloud-desc-begin
 #   Linuxサーバを自分好みに設定します。
 #     スクリプトの実体は https://github.com/ken-washikita/northcave-startup にあります。
-#   ※ このスクリプトは、CentOS7.Xで動作します
+#   ※ このスクリプトは、CentOS8.Xで動作します
 # @sacloud-desc-end
 
 # load & exec
-curl -L https://raw.githubusercontent.com/ken-washikita/northcave-startup/master/startup.sh | bash
+curl -L https://raw.githubusercontent.com/ken-washikita/northcave-startup/master/centos8/startup.sh | bash
 
 # end
---
+++ centos8/startup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# install packages
+dnf -y install dnf-utils python3-inotify || exit 1
+
+# locale
+sed -i 's/ja_JP/en_US/' /etc/locale.conf
+
+# PAM
+sed -i '/^UsePAM no/s/no/yes/' /etc/ssh/sshd_config
+
+# logrotate
+sed -i '/^rotate 4$/s/4/10/' /etc/logrotate.conf
+
+# nftables
+sed -i '/inet-filter\.net/s/^# //' /etc/sysconfig/nftables.conf
+systemctl enable nftables
+
+# vi
+cat <<EOL >/root/.vimrc
+set compatible
+syntax off
+set ai
+map g 1G
+map q :e#
+set cpo-=%
+set notitle
+set tabstop=4
+set shiftwidth=4
+set encoding=utf-8
+set fileencodings=utf-8,euc-jp,sjis,cp932
+set ambiwidth=double
+set history=50
+set more
+EOL
+sed -i '/^map q/s/$/\r/' /root/.vimrc
+ln -s /root/.vimrc /root/.virc
+
+# fail2ban nftables
+cat <<EOL >>/etc/fail2ban/jail.d/local.conf
+banaction = nftables-multiport
+banaction_allports = nftables-allports
+EOL
+
+# fail2ban blacklist
+curl -L https://raw.githubusercontent.com/mitchellkrogza/Fail2Ban-Blacklist-JAIL-for-Repeat-Offenders-with-Perma-Extended-Banning/master/filter.d/blacklist.conf > /etc/fail2ban/filter.d/blacklist.conf
+curl -L https://raw.githubusercontent.com/mitchellkrogza/Fail2Ban-Blacklist-JAIL-for-Repeat-Offenders-with-Perma-Extended-Banning/master/action.d/blacklist.conf > /etc/fail2ban/action.d/blacklist.conf
+cat <<EOL >>/etc/fail2ban/jail.local
+
+[blacklist]
+enabled = true
+backend = auto
+logpath  = /var/log/fail2ban.*
+filter = blacklist
+banaction = blacklist
+bantime  = 31536000   ; 1 year
+findtime = 31536000   ; 1 year
+maxretry = 10
+EOL
+touch /etc/fail2ban/ip.blacklist
+
+# end
